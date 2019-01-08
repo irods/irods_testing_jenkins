beb24fd7ac1753197d10990340bd9e51f93c2baa
Jaspreet Gill <jessi.k.gill@gmail.com>
2019-01-08 14:38:28 -0500

    script that sets up global variables in jenkins

---
+++ config/add-global-environment-variables.groovy
@@ -0,0 +1,21 @@
+import hudson.slaves.EnvironmentVariablesNodeProperty
+import jenkins.model.Jenkins
+
+instance = Jenkins.getInstance()
+globalNodeProperties = instance.getGlobalNodeProperties()
+envVarsNodePropertyList = globalNodeProperties.getAll(EnvironmentVariablesNodeProperty.class)
+
+newEnvVarsNodeProperty = null
+envVars = null
+
+if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
+  newEnvVarsNodeProperty = new EnvironmentVariablesNodeProperty();
+  globalNodeProperties.add(newEnvVarsNodeProperty)
+  envVars = newEnvVarsNodeProperty.getEnvVars()
+} else {
+  envVars = envVarsNodePropertyList.get(0).getEnvVars()
+}
+
+envVars.put("GLOBAL_PARAMETER_DOCKER_REPO", "/irods_docker_files")
+
+instance.save()
