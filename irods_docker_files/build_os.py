4ae87b1f1e10c7ac17c5bfe00f3af1c25d1d0207
Jaspreet Gill <jessi.k.gill@gmail.com>
2019-03-08 13:01:09 -0500

    files required to build multiple os in parallel

---
+++ irods_docker_files/build_os.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+
+import configuration
+import argparse
+import subprocess
+
+def build_os_containers(platform_target, build_id):
+    base_os = configuration.os_identifier_dict[platform_target]
+    dockerfile = 'Dockerfile.ubuntu'
+    if 'centos' in platform_target:
+        dockerfile = 'Dockerfile.centos'
+
+    build_tag = platform_target+':'+build_id
+    docker_cmd = ['docker build -t {0} --build-arg base_image={1} -f {2} .'.format(build_tag, base_os, dockerfile)] 
+    print(docker_cmd)
+    run_build = subprocess.check_call(docker_cmd, shell=True)
+
+def main():
+    parser = argparse.ArgumentParser(description='Build base os-containers')
+    parser.add_argument('-p','--platform_target', type=str, required=True)
+    parser.add_argument('-b','--build_id', type=str, required=True)
+
+    args = parser.parse_args()
+    build_os_containers(args.platform_target, args.build_id)
+   
+if __name__ == '__main__':
+    main()
---
+++ irods_docker_files/configuration.py
@@ -0,0 +1,7 @@
+os_identifier_dict = {
+    'centos_7': 'centos:7',
+    'ubuntu_14': 'ubuntu:14.04',
+    'ubuntu_16': 'ubuntu:16.04',
+    'ubuntu_18': 'ubuntu:18.04'
+}
+
